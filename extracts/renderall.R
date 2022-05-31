4d88482ba4ca88889ea2bd70164e25d17b574b94
david <lilek@tulln.fhwn.ac.at>
2022-05-31 20:26:43 +0200

    updates - 220531

--- extracts/renderall.R
+++ extracts/renderall.R
@@ -1,4 +1,4 @@
 path = "N:/1_A_Bachelor_Master_Intern/00_M_2022/David/Data/evaluation_masterthesis/extracts"
 files <- list.files(path = path, pattern = "*.Rmd")
 
-for (f in files) rmarkdown::render(f)
+for (f in files[-7]) rmarkdown::render(f)
