d73233f5a70f6fe9b1b9baa1195788774c72ecbc
davidlilek <lilek@fhwn.ac.at>
2022-06-06 20:41:07 +0200

    updates

--- UPS1/afterprocessing4masterthesis.R
+++ UPS1/afterprocessing4masterthesis.R
@@ -29,6 +29,7 @@ for (sample_name in sample_names){
 
 ####### comparing via concentration
 res_twopeptides <- readRDS("results_twopeptides.rds")
+#change also file name for png file if you run it with one peptide
 #res_twopeptides <- readRDS("results_onepeptide.rds")
 sample_names <- c("10fmol","25fmol","2fmol","4fmol","50fmol")
 number_proteins <- c()
@@ -102,6 +103,7 @@ plot_no <- ggplot(data=res_2gether, aes(x=Concentration, y=NumberProteins, group
   ylab("Number of Proteins") + 
   xlab("Concentration [fmol]") +
   scale_fill_brewer(palette="Dark2") +
+  labs(color = "Evaluation \n method") +
   theme(legend.position="bottom", legend.box="vertical", legend.margin=margin())
 
 
@@ -110,6 +112,7 @@ plot_rsd <- ggplot(data=res_2gether, aes(x=Concentration, y=rsd, group=Name, col
   geom_point() + 
   ylab("rsd [%]") + 
   xlab("Concentration [fmol]") +
+  labs(color = "Evaluation \n method") +
   theme(legend.position="bottom", legend.box="vertical", legend.margin=margin())
 
 
@@ -127,36 +130,109 @@ ggsave(pth,
 ######################## compare no human proteins 
 # should be 47
 
-########with all values
-sample_names <- c("10fmol","25fmol","2fmol","4fmol","50fmol")
-number_proteins <- c()
-name <- c()
-conc <- c()
-rsd <- c()
-type <- c()
-evaluation <- c()
-for (sample_name in sample_names){
-  tmp <- res_twopeptides[[sample_name]]
-  number_proteins <- append(number_proteins,tmp[,1])
-  name <- append(name,
-                 paste(tmp[,3],"_",tmp[,4], sep = ""))
-  conc <- c(conc, rep(sample_name,6))
-  type <- c(type, paste(tmp[,3]))
-  evaluation <- c(evaluation, paste(tmp[,4]))
-  rsd <- c(rsd, paste(tmp[,2]))
+
+path = "N:/1_A_Bachelor_Master_Intern/00_M_2022/David/Data/evaluation_masterthesis/UPS1/tmpfiles/UPS1_oneuniquepeptide_human/"
+
+##########read in all data with pattern .txt
+# get file list
+file.list <- list.files(path = path, pattern='*.csv')
+# create file path
+file.path <- paste(path,file.list,sep = "")
+# read in files - for each file one variable is created - results are stored in a list
+res_raw <- lapply(file.path, function(i){read.csv2(i, row.names = 1)})
+names(res_raw) <- file.list
+
+i <- 0
+res_number_human_proteins_onepeptide <- c()
+for (i in 1:4){
+  res_number_human_proteins_onepeptide <- c(res_number_human_proteins_onepeptide,colMeans(res_raw[[i]]))
 }
 
 
+##### two peptides
+
+path = "N:/1_A_Bachelor_Master_Intern/00_M_2022/David/Data/evaluation_masterthesis/UPS1/tmpfiles/UPS1_twouniquepeptides_human/"
+
+##########read in all data with pattern .txt
+# get file list
+file.list <- list.files(path = path, pattern='*.csv')
+# create file path
+file.path <- paste(path,file.list,sep = "")
+# read in files - for each file one variable is created - results are stored in a list
+res_raw <- lapply(file.path, function(i){read.csv2(i, row.names = 1)})
+names(res_raw) <- file.list
+
+i <- 0
+res_number_human_proteins_twopeptides <- c()
+for (i in 1:4){
+  res_number_human_proteins_twopeptides <- c(res_number_human_proteins_twopeptides,colMeans(res_raw[[i]]))
+}
+
+type <- c(rep("Razor", 5),
+          rep("Unique", 5),
+          rep("LFQ", 5))
+type <- rep(type,  4)
+type <- rep(type, 2)
+conc <- rep(c(10,25,2,4,50),12)
+conc <- rep(conc, 2)
+peptides <- c(rep("one", 60),
+              rep("two",60))
+evaluationmethod <- c(rep("MBR_pooled",15),
+                      rep("MBR_pooled_oldversion",15),
+                      rep("noMBR_pooled",15),
+                      rep("noMBR_pooled_oldversion",15))
+evaluationmethod <- rep(evaluationmethod, 2)
+
+mean_human_proteins <- c(res_number_human_proteins_onepeptide,res_number_human_proteins_twopeptides)
 
 
+results_numberhumanproteins <- data.frame(mean_human_proteins, type, conc, peptides, evaluationmethod)
+
+A <- results_numberhumanproteins %>% 
+  dplyr::filter(evaluationmethod == "MBR_pooled") %>%
+  ggplot(., aes(x=conc, y=mean_human_proteins, color = type, linetype = peptides)) +
+  geom_line() +
+  geom_point() + 
+  ylab("Number of Proteins") + 
+  xlab("Concentration [fmol]") +
+  theme(legend.position="bottom", legend.box="vertical", legend.margin=margin())
+
+B <- results_numberhumanproteins %>% 
+  dplyr::filter(evaluationmethod == "MBR_pooled_oldversion") %>%
+  ggplot(., aes(x=conc, y=mean_human_proteins, color = type, linetype = peptides)) +
+  geom_line() +
+  geom_point() + 
+  ylab("Number of Proteins") + 
+  xlab("Concentration [fmol]") +
+  theme(legend.position="bottom", legend.box="vertical", legend.margin=margin())
 
 
+C <- results_numberhumanproteins %>% 
+  dplyr::filter(evaluationmethod == "noMBR_pooled") %>%
+  ggplot(., aes(x=conc, y=mean_human_proteins, color = type, linetype = peptides)) +
+  geom_line() +
+  geom_point() + 
+  ylab("Number of Proteins") + 
+  xlab("Concentration [fmol]") +
+  theme(legend.position="bottom", legend.box="vertical", legend.margin=margin())
 
+D <- results_numberhumanproteins %>% 
+  dplyr::filter(evaluationmethod == "noMBR_pooled_oldversion") %>%
+  ggplot(., aes(x=conc, y=mean_human_proteins, color = type, linetype = peptides)) +
+  geom_line() +
+  geom_point() + 
+  ylab("Number of Proteins") + 
+  xlab("Concentration [fmol]") +
+  theme(legend.position="bottom", legend.box="vertical", legend.margin=margin())
 
-
-
-
-
+ggarrange(A,B,C,D, 
+          labels = c("A", "B", "C", "D"),
+          ncol = 2, nrow = 2,
+          common.legend = TRUE, legend="bottom")
+pth <- "N:/1_A_Bachelor_Master_Intern/00_M_2022/David/Data/evaluation_masterthesis/pics/ups1_nohumanproteins.png"
+ggsave(pth,
+       width = 7,
+       height = 7) 
 
 
 ###############################################################
