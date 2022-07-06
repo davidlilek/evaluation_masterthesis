b2158bf82cab66a8f8dcf65448f0434db6133a74
davidlilek <lilek@fhwn.ac.at>
2022-07-06 09:09:37 +0200

    finishing afterprocessing 4 masterthesis extracts

--- extracts/afterprocessing4masterthesis.R
+++ extracts/afterprocessing4masterthesis.R
@@ -4,14 +4,17 @@
 #
 ##########################################
 
-#(1) run 20220406_TR_extract_summary_rerun_two peptides
-#(2) run 20220406_TR_extract_summary_rerun_two peptides
-
 library(ggpubr)
 library(stringr)
 library(dplyr)
 library(ggplot2)
 
+
+############################
+#
+# plots for base evaluation
+#
+############################
 plots_no_twopeptides <- readRDS("plots_no_twopeptides.rds")
 plots_rsd_twopeptides <- readRDS("plots_rsd_twopeptides.rds")
 plots_no_onepeptide <- readRDS("plots_no_onepeptide.rds")
@@ -32,7 +35,61 @@ for (sample_name in sample_names){
          height = 7)  
 }
 
-######## for second peptide analysis
+### combine A-B
+
+sample_name1 <- "_A"
+sample_name2 <- "_B"
+ggarrange(plots_no_onepeptide[[sample_name1]],plots_no_twopeptides[[sample_name1]], plots_rsd_onepeptide[[sample_name1]] ,plots_rsd_twopeptides[[sample_name1]], 
+          plots_no_onepeptide[[sample_name2]],plots_no_twopeptides[[sample_name2]], plots_rsd_onepeptide[[sample_name2]] ,plots_rsd_twopeptides[[sample_name2]], 
+          labels = c("A", "B", "C", "D","a","b","c","d"),
+          ncol = 2, nrow = 4,
+          common.legend = TRUE, legend="top",
+          hjust = c(rep(-4.5,4),rep(-5.25,4)),
+          align = "hv")
+pth <- paste("N:/1_A_Bachelor_Master_Intern/00_M_2022/David/Data/evaluation_masterthesis/pics/extracts_rerun_compare",sample_name1,sample_name2,".png",sep="")
+ggsave(pth,
+       width = 7,
+       height = 7)  
+
+### combine C-D
+
+sample_name1 <- "_C"
+sample_name2 <- "_D"
+ggarrange(plots_no_onepeptide[[sample_name1]],plots_no_twopeptides[[sample_name1]], plots_rsd_onepeptide[[sample_name1]] ,plots_rsd_twopeptides[[sample_name1]], 
+          plots_no_onepeptide[[sample_name2]],plots_no_twopeptides[[sample_name2]], plots_rsd_onepeptide[[sample_name2]] ,plots_rsd_twopeptides[[sample_name2]], 
+          labels = c("A", "B", "C", "D","a","b","c","d"),
+          ncol = 2, nrow = 4,
+          common.legend = TRUE, legend="top",
+          hjust = c(rep(-4.5,4),rep(-5.25,4)),
+          align = "hv")
+pth <- paste("N:/1_A_Bachelor_Master_Intern/00_M_2022/David/Data/evaluation_masterthesis/pics/extracts_rerun_compare",sample_name1,sample_name2,".png",sep="")
+ggsave(pth,
+       width = 7,
+       height = 7)  
+
+### combine E-F
+
+sample_name1 <- "_E"
+sample_name2 <- "_F"
+ggarrange(plots_no_onepeptide[[sample_name1]],plots_no_twopeptides[[sample_name1]], plots_rsd_onepeptide[[sample_name1]] ,plots_rsd_twopeptides[[sample_name1]], 
+          plots_no_onepeptide[[sample_name2]],plots_no_twopeptides[[sample_name2]], plots_rsd_onepeptide[[sample_name2]] ,plots_rsd_twopeptides[[sample_name2]], 
+          labels = c("A", "B", "C", "D","a","b","c","d"),
+          ncol = 2, nrow = 4,
+          common.legend = TRUE, legend="top",
+          hjust = c(rep(-4.5,4),rep(-5.25,4)),
+          align = "hv")
+pth <- paste("N:/1_A_Bachelor_Master_Intern/00_M_2022/David/Data/evaluation_masterthesis/pics/extracts_rerun_compare",sample_name1,sample_name2,".png",sep="")
+ggsave(pth,
+       width = 7,
+       height = 7)  
+
+
+
+############################
+#
+# plots for second peptides
+#
+############################
 
 plots_no_twopeptides <- readRDS("plots_no_secondpeptides_twopeptides.rds")
 plots_rsd_twopeptides <- readRDS("plots_rsd_secondpeptides_twopeptides.rds")
@@ -53,29 +110,63 @@ for (sample_name in sample_names){
 }
 
 
-
-
 ### combine A-B
 
-sample_name1 <- "_C"
-sample_name2 <- "_D"
+sample_name1 <- "_A"
+sample_name2 <- "_B"
 ggarrange(plots_no_onepeptide[[sample_name1]],plots_no_twopeptides[[sample_name1]], plots_rsd_onepeptide[[sample_name1]] ,plots_rsd_twopeptides[[sample_name1]], 
-         plots_no_onepeptide[[sample_name2]],plots_no_twopeptides[[sample_name2]], plots_rsd_onepeptide[[sample_name2]] ,plots_rsd_twopeptides[[sample_name2]], 
-         labels = c("A", "B", "C", "D","a","b","c","d"),
-         ncol = 2, nrow = 4,
-         common.legend = TRUE, legend="top",
-         hjust = c(rep(-4.5,4),rep(-5.25,4)),
-         align = "hv")
+          plots_no_onepeptide[[sample_name2]],plots_no_twopeptides[[sample_name2]], plots_rsd_onepeptide[[sample_name2]] ,plots_rsd_twopeptides[[sample_name2]], 
+          labels = c("A", "B", "C", "D","a","b","c","d"),
+          ncol = 2, nrow = 4,
+          common.legend = TRUE, legend="top",
+          hjust = c(rep(-4.5,4),rep(-5.25,4)),
+          align = "hv")
 pth <- paste("N:/1_A_Bachelor_Master_Intern/00_M_2022/David/Data/evaluation_masterthesis/pics/extracts_rerun_secondpeptides_compare",sample_name1,sample_name2,".png",sep="")
 ggsave(pth,
        width = 7,
        height = 7)  
 
+### combine C-D
+
+sample_name1 <- "_C"
+sample_name2 <- "_D"
+ggarrange(plots_no_onepeptide[[sample_name1]],plots_no_twopeptides[[sample_name1]], plots_rsd_onepeptide[[sample_name1]] ,plots_rsd_twopeptides[[sample_name1]], 
+          plots_no_onepeptide[[sample_name2]],plots_no_twopeptides[[sample_name2]], plots_rsd_onepeptide[[sample_name2]] ,plots_rsd_twopeptides[[sample_name2]], 
+          labels = c("A", "B", "C", "D","a","b","c","d"),
+          ncol = 2, nrow = 4,
+          common.legend = TRUE, legend="top",
+          hjust = c(rep(-4.5,4),rep(-5.25,4)),
+          align = "hv")
+pth <- paste("N:/1_A_Bachelor_Master_Intern/00_M_2022/David/Data/evaluation_masterthesis/pics/extracts_rerun_secondpeptides_compare",sample_name1,sample_name2,".png",sep="")
+ggsave(pth,
+       width = 7,
+       height = 7)  
+
+### combine E-F
+
+sample_name1 <- "_E"
+sample_name2 <- "_F"
+ggarrange(plots_no_onepeptide[[sample_name1]],plots_no_twopeptides[[sample_name1]], plots_rsd_onepeptide[[sample_name1]] ,plots_rsd_twopeptides[[sample_name1]], 
+          plots_no_onepeptide[[sample_name2]],plots_no_twopeptides[[sample_name2]], plots_rsd_onepeptide[[sample_name2]] ,plots_rsd_twopeptides[[sample_name2]], 
+          labels = c("A", "B", "C", "D","a","b","c","d"),
+          ncol = 2, nrow = 4,
+          common.legend = TRUE, legend="top",
+          hjust = c(rep(-4.5,4),rep(-5.25,4)),
+          align = "hv")
+pth <- paste("N:/1_A_Bachelor_Master_Intern/00_M_2022/David/Data/evaluation_masterthesis/pics/extracts_rerun_secondpeptides_compare",sample_name1,sample_name2,".png",sep="")
+ggsave(pth,
+       width = 7,
+       height = 7) 
 
 
-################# compare MBR no MBR
 
-# one peptide
+############################
+#
+# compare MBR no MBR
+#
+############################
+
+# load and arrange data for one peptide
 sample_names <- LETTERS[1:6]
 res_2gether_onepeptide <- readRDS("results_onepeptide.rds")
 res_2gether_onepeptide <- as.data.frame(do.call(rbind,res_2gether_onepeptide))
@@ -88,7 +179,7 @@ summary(noMBR$rsd)
 MBR_comparison$comparison <- ((MBR_comparison$no/noMBR$no)-1)*100
 MBR_comparison_onepeptide <- MBR_comparison
 
-# two peptides
+# load and arrange data for two peptides
 res_2gether_twopeptides <- readRDS("results_twopeptides.rds")
 res_2gether_twopeptides <- as.data.frame(do.call(rbind,res_2gether_twopeptides))
 res_2gether_twopeptides$sample_name <- rep(sample_names,each=18)
@@ -101,60 +192,26 @@ MBR_comparison$comparison <- ((MBR_comparison$no/noMBR$no)-1)*100
 MBR_comparison_twopeptides <- MBR_comparison
 
 
-# bring the 2gether
+# bring them 2gether
 MBR_comparison <- rbind(MBR_comparison_onepeptide,MBR_comparison_twopeptides)
 MBR_comparison$peptides <- rep(c("one","two"),each=54)
 
+# how much jitter on the x-axis
+dodge <- position_dodge(.3)
 
- ggplot(MBR_comparison, aes(x=Type, y=comparison, color=Evaluation)) +
-  geom_boxplot() +
+ggplot(MBR_comparison, aes(x=Type, y=comparison, color=Evaluation, shape=peptides)) +
+  geom_point(position = dodge, size = 2.5) +
+  scale_shape_manual(values=c(1, 16)) +
   theme(legend.position="bottom") +
   theme(legend.key.size = unit(0.15, 'cm')) +
   guides(fill=guide_legend(nrow=2, byrow=TRUE)) +
   ylab("Proportion of more proteins\nfound using MBR [%]") +
   labs(fill = "Evaluation \n method") + 
   facet_wrap(~sample_name, ncol=2) +
-  scale_fill_brewer(palette="Dark2", labels = c("pooled\nMaxQuant","pooled\nMaxQuant oldversion","manually\npooled","manually\npooled")) 
-
- 
- p_plot_one
-p_plot_one <- ggplot(MBR_comparison, aes(x=Type, y=comparison, fill=Evaluation)) +
-  geom_bar(stat='identity', position=position_dodge()) +
-  theme_minimal() +
   scale_fill_brewer(palette="Dark2", labels = c("pooled\nMaxQuant","pooled\nMaxQuant oldversion","manually\npooled","manually\npooled")) +
-  theme(legend.position="bottom") +
-  theme(legend.key.size = unit(0.15, 'cm')) +
-  guides(fill=guide_legend(nrow=2, byrow=TRUE)) +
-  ylab("Proportion of more proteins\nfound using MBR [%]") +
-  labs(fill = "Evaluation \n method") 
+  scale_color_manual(values = c("#1B9E77","#D95F02","#7570B3")) 
 
-p_plot_one
-
-
-
-
-
-p_plot_two <- ggplot(MBR_comparison, aes(x=Type, y=comparison, fill=Evaluation)) +
-  geom_bar(stat='identity', position=position_dodge()) +
-  theme_minimal() +
-  scale_fill_brewer(palette="Dark2", labels = c("pooled\nMaxQuant","pooled\nMaxQuant oldversion","manually\npooled","manually\npooled")) +
-  theme(legend.position="bottom") +
-  theme(legend.key.size = unit(0.15, 'cm')) +
-  guides(fill=guide_legend(nrow=2, byrow=TRUE)) +
-  ylab("Proportion of more proteins\nfound using MBR [%]") +
-  labs(fill = "Evaluation \n method")
-
-p_plot_two
-
-
-# mbr no mbr
-ggarrange(
-          labels = c("A", "B"),
-          ncol = 2, nrow = 4,
-          common.legend = TRUE, legend="top",
-          hjust = c(rep(-4.5,4),rep(-5.25,4)),
-          align = "hv")
-pth <- paste("N:/1_A_Bachelor_Master_Intern/00_M_2022/David/Data/evaluation_masterthesis/pics/extracts_rerun_secondpeptides_compare",sample_name1,sample_name2,".png",sep="")
+pth <- paste("N:/1_A_Bachelor_Master_Intern/00_M_2022/David/Data/evaluation_masterthesis/pics/extracts_rerun_comparisonmbr.png")
 ggsave(pth,
        width = 7,
        height = 7) 
